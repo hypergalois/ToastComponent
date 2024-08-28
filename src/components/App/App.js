2a5cb26c0d2e81b4f96e57fe1e67edaad6f87976
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-08-28 14:07:39 +0200

    Wiring up textarea and radio buttons

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import ToastPlayground from '../ToastPlayground';
-import Footer from '../Footer';
+import ToastPlayground from "../ToastPlayground";
+import Footer from "../Footer";
 
 function App() {
-  return (
-    <>
-      <ToastPlayground />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<ToastPlayground />
+		</>
+	);
 }
 
 export default App;
