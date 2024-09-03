4ae7631a3dd2c87af571c443faeb25eca7bab6a6
hypermoderngalois <72045733+hypermoderngalois@users.noreply.github.com>
2024-09-03 17:42:12 +0200

    Added Portal for improved resiliency

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
 import Toast from "../Toast";
 import { useToast } from "../../context/ToastProvider";
@@ -8,14 +9,15 @@ import styles from "./ToastShelf.module.css";
 function ToastShelf() {
 	const { toasts, handleDismissToast } = useToast();
 
-	return (
+	return createPortal(
 		<ol className={styles.wrapper} role="region" aria-live="polite" aria-label="Notification">
 			{toasts.map((toast) => (
 				<li className={styles.toastWrapper} key={toast.id}>
 					<Toast key={toast.id} message={toast.message} variant={toast.variant} dissmissToast={() => handleDismissToast(toast.id)} />
 				</li>
 			))}
-		</ol>
+		</ol>,
+		document.querySelector("#toast-root")
 	);
 }
 
